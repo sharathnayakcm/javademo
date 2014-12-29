9ba44f95cd4756d30cb2c5dbfffdb3ab1b1ec08e
sharath <sharath.n@sonata-software.com>
2014-12-29 19:15:01 +0530

    commented the flyway part

--- src/main/java/foo/bar/App.java
+++ src/main/java/foo/bar/App.java
@@ -8,10 +8,12 @@ public class App
     public static void main( String[] args )
     {
     	
-    	Flyway flyway = new Flyway();
+//    	Flyway flyway = new Flyway();
+//        
+//        flyway.setDataSource("jdbc:mysql://localhost:3306/flyjava2", "root", "root");
+//        flyway.migrate();
+        
         System.out.println( "Hello World!" );
-        flyway.setDataSource("jdbc:mysql://localhost:3306/flyjava2", "root", "root");
-        flyway.migrate();
         
     }
 }
