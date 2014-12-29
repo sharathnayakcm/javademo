4eec0f0be810a20d8cdd9ab1a85aaa09fcbf2b94
sharath <sharath.n@sonata-software.com>
2014-12-29 18:26:37 +0530

    added space to file

--- src/main/java/foo/bar/App.java
+++ src/main/java/foo/bar/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "Hello World!" );
         flyway.setDataSource("jdbc:mysql://localhost:3306/flyjava2", "root", "root");
         flyway.migrate();
+        
     }
 }
