90b4106bca0da949b0144119da33514d7adc4aaf
sharath <sharath.n@sonata-software.com>
2014-12-29 19:23:10 +0530

    added flyway

--- src/main/java/foo/bar/App.java
+++ src/main/java/foo/bar/App.java
@@ -8,9 +8,9 @@ public class App
     public static void main( String[] args )
     {
     	
-//    	Flyway flyway = new Flyway();
-//        
-//        flyway.setDataSource("jdbc:mysql://localhost:3306/flyjava2", "root", "root");
+    	Flyway flyway = new Flyway();
+        
+        flyway.setDataSource("jdbc:mysql://localhost:3306/flyjava2", "root", "root");
 //        flyway.migrate();
         
         System.out.println( "Hello World!" );
